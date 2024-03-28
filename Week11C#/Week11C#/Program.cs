cdeb520a1676a661152052db0065d3cd31105ad3
Abhishamathew <abhisha.mathew2k17@gmail.com>
2024-03-28 12:11:13 -0400

    Commit

---
+++ Week11C#/Week11C#/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week11C_.Models.Entities;
+
+namespace Week11CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbContext = new MMABooksContext();
+
+            var customers = dbContext.Customers;
+
+            // Display the customers
+            foreach (var customer in customers)
+
+            {
+
+                Console.WriteLine(customer.Name);
+
+            }
+        }
+    }
+}
+
+
